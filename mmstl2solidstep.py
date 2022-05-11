68135a451443ddc285377c861ac66ddd34c28236
Christian Peco <christian.peco@psu.edu>
2022-05-11 00:34:26 -0400

    Update mmstl2solidstep.py

--- mmstl2solidstep.py
+++ mmstl2solidstep.py
@@ -12,22 +12,18 @@ stl_name = sys.argv[2]
 object_name = stl_name[:-4]
 print('mmstl2solid is processing file: ', stl_name, 'object', object_name)
 
-Mesh.open(stl_name)
 
-print("STL mesh opened...")
+# Macro Init +++++++++++++++++++++++++++++++++++++++++++++++++
 
-string_part = object_name + 'temp'
-FreeCAD.getDocument("Unnamed").addObject("Part::Feature", string_part)
-__shape__ = Part.Shape()
-__shape__.makeShapeFromMesh(FreeCAD.getDocument("Unnamed").getObject(object_name).Mesh.Topology, 0.100000)
-FreeCAD.getDocument("Unnamed").getObject(string_part).Shape=__shape__
-FreeCAD.getDocument("Unnamed").getObject(string_part).purgeTouched()
+mesh = Mesh.Mesh(stl_name)
+shape = Part.Shape()
+shape.makeShapeFromMesh(mesh.Topology, 0.1)
 
-solid = Part.makeSolid(__shape__)
+solid = Part.makeSolid(shape)
 string_solid = object_name + '_solid.step'
 solid.exportStep(string_solid)
 
-del __shape__
+del shape
 
 # Macro End +++++++++++++++++++++++++++++++++++++++++++++++++
 
